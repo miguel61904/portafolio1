79e61668ff686cfe9a9eebf29d09b1aca71e0885
miguel61904 <75705385+miguel61904@users.noreply.github.com>
2021-06-07 22:53:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ assets/js/menu.js
@@ -0,0 +1,9 @@
+let responsivoMenu = document.getElementsByID('boton-de-inicio');
+let mainMenu = document.getElementById('main-menu');
+let meunuflag = false;
+//eventos
+responsivoMenu.addEventListener("click",function({ //evento de clicl
+  mainMenu.setAttribute("class", "abrirmenu-responsivo");//funcion es lo que va a hacer el evento
+ alert("haspre");
+
+});
